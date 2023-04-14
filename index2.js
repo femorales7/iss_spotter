883ddc4386d1a1c0bda428415a81b19f55792bc3
Your Name <your@email.com>
2023-04-14 10:25:14 -0600

    implementation of promises with these to files

---
+++ index2.js
@@ -0,0 +1,21 @@
+const {fetchMyIP} = require("./iss_promised");
+const {fetchCoordsByIP} = require("./iss_promised");
+const {fetchISSFlyOverTimes } = require("./iss_promised");
+const {nextISSTimesForMyLocation} = require('./iss_promised');
+
+// fetchMyIP()
+// .then (fetchCoordsByIP)
+// .then (fetchISSFlyOverTimes)
+// .then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+.then ((passTimes) => {
+  console.log(passTimes);
+})
+.catch((error) => {
+  console.log("It didn't work: ", error.message);
+});
+
+
+  
+
